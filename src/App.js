ca9f8c319923856b6374253806937d96a80f228c
Harryson Morais <harrysonmorais19@gmail.com>
2020-09-25 01:10:18 -0300

    Eatado e Imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header'
 
 function App() {
+    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+    function handleAddProject() {
+        //projects.push(`Novo Projeto ${Date.now()}`);
+        setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+    }
+
     return (
         <>
-            <Header />;
-            <Header />;
+            <Header title="Projects" />
+
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
         </>
     );
 }
